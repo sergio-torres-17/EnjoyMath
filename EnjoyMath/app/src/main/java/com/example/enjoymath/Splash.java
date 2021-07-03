44114c6a4133a43db3358aacea5df072ba1b511c
ana3argote <86811574+ana3argote@users.noreply.github.com>
2021-07-03 17:54:00 -0500

    nuevo
    ahí está

--- EnjoyMath/app/src/main/java/com/example/enjoymath/Splash.java
+++ EnjoyMath/app/src/main/java/com/example/enjoymath/Splash.java
@@ -2,6 +2,7 @@ package com.example.enjoymath;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
@@ -10,5 +11,19 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Thread hilo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(Splash.this,MenuPrincipal.class));
+                finish();
+            }
+        });
+        hilo.start();
+
     }
 }
