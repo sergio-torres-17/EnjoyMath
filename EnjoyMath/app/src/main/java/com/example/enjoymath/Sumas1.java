a71e3425cbb993bc9b0d05128eff167b3d2cb5bc
ana3argote <86811574+ana3argote@users.noreply.github.com>
2021-07-03 18:07:29 -0500

    otro cambio
    jejej

---
+++ EnjoyMath/app/src/main/java/com/example/enjoymath/Sumas1.java
@@ -0,0 +1,14 @@
+package com.example.enjoymath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sumas1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sumas1);
+    }
+}
---
+++ EnjoyMath/app/src/main/java/com/example/enjoymath/Sumas2.java
@@ -0,0 +1,14 @@
+package com.example.enjoymath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sumas2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sumas2);
+    }
+}
